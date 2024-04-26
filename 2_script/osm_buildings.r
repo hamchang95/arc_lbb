03b47660e3e5d8952f4db3676c56e6031b7afb90
hamchang <alice9501@gmail.com>
2024-04-26 10:24:17 +0100

    Test commit

---
+++ 2_script/osm_buildings.r
@@ -0,0 +1,23 @@
+########## SET UP ##########
+#--Clear enviornment
+rm(list = ls())
+
+#--Install / load packages
+pacman::p_load(sf, httr, jsonlite, here, tmap, osmdata, tidyverse, data.table, rmapshaper, xml2, rvest)
+
+#--Get Barnet polygon
+bnt_bb <- getbb("London Borough of Barnet", format_out = "polygon")
+
+#--Get Barnet boudnary
+bnt_bdry <- opq(bnt_bb) |> 
+    add_osm_feature(key = "admin_level",value = 8) |>
+    osmdata_sf()
+
+#--Check the output 
+ggplot(bnt_bdry$osm_multipolygons) +
+    geom_sf()
+    # it looks alright but it also returns other contingent boroughs
+    # this is probably there will be some points or lines on the intersection 
+    # will first download attributes data and filter out later on
+
+
